2c5504b1174e3f27814e83f78f85b75f58bd4a25
Andrea <andrea88@live.se>
2021-01-29 10:45:49 +0100

    updated endpoints

---
+++ models/index.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv"
+import mongoose from 'mongoose'
+
+// This file is opened when you are importing a whole folder like this './models' 
+// Here all connection to the db magic happens
+
+dotenv.config()
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/enBostad"
+mongoose.set('useCreateIndex', true)
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+
+mongoose.Promise = Promise
+
+
+// including models from the external files - this makes it easier to add/delete/update model if it is needed
+module.exports.User = require('./user')
