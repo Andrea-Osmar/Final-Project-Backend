4426620591997bc3a0e26a22ac6d027fb19fe47f
Andrea Osmar <63371909+Andrea-Osmar@users.noreply.github.com>
2021-03-02 09:40:21 +0100

    Update index.js

--- models/index.js
+++ models/index.js
@@ -6,8 +6,8 @@ dotenv.config()
 //__________ Database Code
 const mongoUrl =
 	process.env.MONGO_URL ||
-	'mongodb+srv://andreao:SLXqQJV5LDq3OW9w@cluster0.z2q0p.mongodb.net/enBostad?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, })
+	`mongodb+srv://${process.env.DB_USER}:${process.env.DB_KEY}@cluster0.qxpka.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 
 mongoose.Promise = Promise
 
